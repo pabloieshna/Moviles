deedb11a341b1da81089fa649f49dff207adc270
jesuscieshna <jchaveroc01@iesarroyoharnina.es>
2024-01-22 10:37:37 +0100

    inital commit

---
+++ Moviles/src/main/java/com/moviles/MovilesApplication.java
@@ -0,0 +1,13 @@
+package com.moviles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovilesApplication.class, args);
+	}
+
+}
